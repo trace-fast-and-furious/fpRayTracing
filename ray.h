3d0731851d677ff8baa2ca8358e8dd34ec7f615c
정은수 <80330331+intsoo@users.noreply.github.com>
2022-12-09 18:46:06 +0900

    Add files via upload

---
+++ ray.h
@@ -0,0 +1,45 @@
+/*
+ * ===================================================
+ *
+ *       Filename:  ray.h
+ *    Description:  Ray Tracing: The Next Week (RTTNW): ~BVH 
+ *        Created:  2022/07/13
+ * 
+ * ===================================================
+ */
+
+
+// Preprocessors
+
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+
+// Classes
+
+class ray {
+public:
+    	ray() {}
+  	ray(const point3& origin, const vec3& direction, float time = 0.0)
+      		: orig(origin), dir(direction), tm(time)
+	{}
+
+    	point3 origin() const { return orig; }
+    	vec3 direction() const { return dir; }
+        float time() const    { return tm; }
+
+    	point3 at(float t) const {
+		return orig + t * dir;
+    	}
+
+public:
+    	point3 orig;
+    	vec3 dir;
+        float tm;
+};
+
+
+#endif
+
