f0097b865d1a4f404fd9aed249c268e317b4fbcd
Yeonhee <spig0126@ewhain.net>
2023-03-24 14:21:14 +0900

    🔖 FP32 based ray tracer(CPU, with BVH)

---
+++ sphere_scene/fp32/with_BVH/Makefile
@@ -0,0 +1,7 @@
+CC=nvcc 
+
+RTTNW.out:  mkClockMeasure.h  mkCuda.h main.cu 
+	$(CC) -g -G -o RTTNW.out main.cu
+
+clean:
+	rm -rf RTTNW.out
---
+++ sphere_scene/fp32/with_BVH/color.h
@@ -0,0 +1,38 @@
+/*
+ * ===================================================
+ *
+ *       Filename:  color.h (NOT USED IN THIS VERSION)
+ *    Description:  Ray Tracing: The Next Week (RTTNW): ~BVH
+ *        Created:  2022/07/13
+ *
+ * ===================================================
+ */
+
+
+// Preprocessors
+
+#ifndef COLOR_H
+#define COLOR_H
+#include "vec3.h"
+#include <iostream>
+
+
+// Functions
+
+void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
+    	auto r = pixel_color.x();
+    	auto g = pixel_color.y();
+    	auto b = pixel_color.z();
+
+    	auto scale = 1.0 / samples_per_pixel;
+    	r = sqrt(scale * r);
+    	g = sqrt(scale * g);
+    	b = sqrt(scale * b);
+
+    	out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+		<< static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+		<< static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
+}
+
+
+#endif
